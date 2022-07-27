2115c8beaec44acfb3bcaed70b1016ece3dd6990
Amushkaa <ameee.am@gmail.com>
2022-07-27 16:38:49 +0300

    created: responsive-menu

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ const swiper = new Swiper('.swiper', {
     delay: 2700,
   },
 });
+
+const menuButton = document.querySelector(".header-menu__mobile")
+menuButton.addEventListener('click', function() {
+  document.querySelector ('.header-right').classList.toggle("header-right--visible");
+  document.querySelector ('.header-menu__close').classList.toggle("header-menu__close--visible");
+  document.querySelector ('.header-menu__button').classList.toggle("header-menu__button--hidden");
+  document.querySelector ('.header').classList.toggle("header--fixed");
+
+});
