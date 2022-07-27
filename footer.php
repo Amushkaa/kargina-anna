e36df4bd3c8928006485e0f4b02073b327ef9014
Amushkaa <ameee.am@gmail.com>
2022-07-27 20:39:27 +0300

    created: projects page

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer-wrapper">
+            <a class="footer__link" href="#">ameee.am@gmail.com</a>
+            <span class="footer__text">© Anna Kargina, 2022</span>
+        </div>
+    </div>
+</footer>
+<script src="js/swiper-bundle.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ projects.php
@@ -0,0 +1,35 @@
+<?php include ('header.php')?>
+<div class="choice">
+    <div class="container">
+        <div class="choice-wrapper">
+            <div class="choice-line choice-line__luxoft">
+                <img src="img/projects/luxoft.png" alt="Image: Luxoft" class="choice__image">
+                <a href="#" class="choice__tittle">Luxoft</a>
+                <img src="img/projects/arrow.svg" alt="Icon: Arrow" class="choice__arrow">
+            </div>
+            <div class="choice-line choice-line__globe">
+                <img src="img/projects/globe.png" alt="Image: The globe and Mail" class="choice__image">
+                <a href="#" class="choice__tittle">The Globe and Mail</a>
+                <img src="img/projects/arrow.svg" alt="Icon: Arrow" class="choice__arrow">
+            </div>
+            <div class="choice-line choice-line__sheridan">
+                <img src="img/projects/sheridan.png" alt="Image: Sheridan Nurseries" class="choice__image">
+                <a href="#" class="choice__tittle">Sheridan Nurseries</a>
+                <img src="img/projects/arrow.svg" alt="Icon: Arrow" class="choice__arrow">
+            </div>
+            <div class="choice-line choice-line__winestyle">
+                <img src="img/projects/winestyle.png" alt="Image: Winestyle" class="choice__image">
+                <a href="#" class="choice__tittle">Winestyle</a>
+                <img src="img/projects/arrow.svg" alt="Icon: Arrow" class="choice__arrow">
+            </div>
+            <div class="choice-line choice-line__insea">
+                <img src="img/projects/insea.png" alt="Image: Insea" class="choice__image">
+                <a href="#" class="choice__tittle">Insea</a>
+                <img src="img/projects/arrow.svg" alt="Icon: Arrow" class="choice__arrow">
+            </div>
+        </div>
+    </div>
+<!-- container -->
+</div>
+<!-- project-choice -->
+<?php include ('footer.php')?>
