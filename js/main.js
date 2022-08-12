9009a382b1d49a93c28ff83db3cae34cace2f8bc
Amushkaa <ameee.am@gmail.com>
2022-08-12 23:24:38 +0300

    created: luxoft page

--- js/main.js
+++ js/main.js
@@ -18,5 +18,17 @@ menuButton.addEventListener('click', function() {
   document.querySelector ('.header-menu-mobile__close').classList.toggle("header-menu-mobile__close--visible");
   document.querySelector ('.header-menu-mobile__button').classList.toggle("header-menu-mobile__button--hidden");
   document.querySelector ('.header').classList.toggle("header--fixed");
-
 });
+
+
+const modalButton = document.querySelector(".contacts__button")
+  modalButton.addEventListener('click', function() {
+    document.querySelector ('.modal__overlay').classList.toggle("modal__overlay--visible");
+    document.querySelector ('.modal__dialog').classList.toggle("modal__dialog--visible");
+  });
+
+const closeModal = document.querySelector(".modal__close")
+  modalButton.addEventListener('click', function() {
+    document.querySelector ('.modal__overlay').classList.toggle("modal__overlay--visible");
+    document.querySelector ('.modal__dialog').classList.toggle("modal__dialog--visible");
+  });
